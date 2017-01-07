d740e7298051f0b0ba86e205315fcdb80a6587a6
Michael Vashchinsky <michaelv@sparkbeyond.com>
2017-01-07 20:43:19 +0200

    Add ';'

--- routes/builds.js
+++ routes/builds.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const config  = require('../config.json');
-const jenkins = require("../service/jenkinsLib")
+const jenkins = require("../service/jenkinsLib");
 
 
 /* GET users listing. */
