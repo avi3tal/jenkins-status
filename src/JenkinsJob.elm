5a3e89590d549b9d3eb9832e85efa0fbf8b48d5b
Michael Vashchinsky <michaelv@sparkbeyond.com>
2017-01-07 21:54:15 +0200

    Job.timestamp is now Date. Format Job.timestamp. Format Job.downstream title.

--- src/JenkinsJob.elm
+++ src/JenkinsJob.elm
@@ -2,6 +2,8 @@ module JenkinsJob exposing (..)
 
 -- MODEL
 
+import Date exposing (Date)
+import Date.Extra as Date
 import Html exposing (Html, div, table, tbody, td, text, th, tr)
 import Html.Attributes exposing (class, style)
 import Json.Decode as Decode exposing (Decoder)
@@ -11,7 +13,7 @@ import Json.Decode.Pipeline exposing (decode, required)
 type alias Job =
     { name : String
     , color : String
-    , timestamp : Int
+    , timestamp : Date
     , downstream : DownStreams
     , building : Bool
     }
@@ -40,14 +42,33 @@ view job =
                 ]
             , tr []
                 [ th [] [ text "Started On" ]
-                , td [] [ text <| toString job.timestamp ]
+                , td [] [ text <| formatDate job.timestamp ]
                 ]
             , tr []
-                [ th [] [ text "Down Stream Jobs" ]
+                [ th [] [ text <| formatDownStreamTitle job.downstream ]
                 , td [] (downStreamsView job.downstream)
                 ]
             ]
         ]
+
+
+formatDownStreamTitle : DownStreams -> String
+formatDownStreamTitle downStreams =
+    let
+        suffix =
+            case downStreams of
+                DownStreams jobs ->
+                    if List.length jobs == 0 then
+                        ""
+                    else
+                        " (" ++ (toString <| List.length jobs) ++ ")"
+    in
+        "Down Stream Jobs" ++ suffix
+
+
+formatDate : Date -> String
+formatDate date =
+    Date.toUtcFormattedString "MMM dd, yyyy HH:mm:ss X" date
 
 
 downStreamsView : DownStreams -> List (Html msg)
@@ -74,11 +95,17 @@ downStreamDecoder =
     Decode.map DownStreams (Decode.list (Decode.lazy (\_ -> jobDecoder)))
 
 
+dateFromTimestampDecoder : Decoder Date
+dateFromTimestampDecoder =
+    Decode.map toFloat Decode.int
+        |> Decode.map Date.fromTime
+
+
 jobDecoder : Decoder Job
 jobDecoder =
     decode Job
         |> required "name" Decode.string
         |> required "color" Decode.string
-        |> required "timestamp" Decode.int
+        |> required "timestamp" dateFromTimestampDecoder
         |> required "downstream" downStreamDecoder
         |> required "building" Decode.bool
