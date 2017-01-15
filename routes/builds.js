ad8446948640afff5d1d69e2f9bd3741274e682c
Michael Vashchinsky <michaelv@sparkbeyond.com>
2017-01-15 23:34:31 +0200

    Use Promise on backend and make `jenkinsLib.js` to read config instead of `build.js` route

--- routes/builds.js
+++ routes/builds.js
@@ -6,166 +6,17 @@ const jenkins = require("../service/jenkinsLib");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-
-	// TODO remove if condition
-	// allow working only with real environments
-	// or using External Mocker
-	if(!config.realEnv)
-		res.send([
-			{name: "master-1111", number: 1111},
-			{name: "RC-3333", number: 3333},
-			{name: "master-2222", number: 2222},
-			{name: "master-4444", number: 4444}
-		]);
-	else
-		jenkins.listBuilds(10, (item) => {
-			res.send(item)
-		});
+	jenkins.listBuilds(10).then(
+		builds => res.send(builds),
+		err => next(err)
+	);
 });
 
 router.get('/:id', function(req, res, next) {
-	jenkins.listJobsByBuild(req.params.id);
-
-	let j = {
-		displayName: `Build-${req.params.id}`,
-		number: req.params.id,
-		jobs: [
-			{
-				name: "Build",
-				color: "red",
-				timestamp: 1483693575274,
-				downstream: [],
-				building: false
-			},
-			{
-				name: "Smoke",
-			 	color: "blue",
-				building: false,
-				timestamp: 1483693575274,
-				downstream: [
-					{
-						name: "TestFast",
-						color: "blue",
-						building: false,
-						timestamp: 1483693575274,
-						downstream: []
-					},
-					{
-						name: "RunE2E",
-						color: "blue",
-						building: false,
-						timestamp: 1483693575274,
-						downstream: [
-							{
-								name: "Migration-Server-Upgrade",
-								color: "blue",
-								building: false,
-								timestamp: 1483693575274,
-								downstream: []
-							},
-							{
-								name: "testkit",
-								color: "red",
-								building: false,
-								timestamp: 1483693575274,
-								downstream: []
-							}
-						]
-					}
-				]
-			},
-			{
-				name: "PostSmoke",
-				color: "red",
-				building: true,
-				timestamp: 1483693575274,
-				downstream: [
-					{
-						name: "DployAndTest",
-						color: "blue",
-						building: true,
-						timestamp: 1483693575274,
-						downstream: []
-					},
-					{
-						name: "RunE2E",
-						color: "blue",
-						building: false,
-						timestamp: 1483693575274,
-						downstream: [
-							{
-								name: "BuildInstance",
-								color: "blue",
-								building: false,
-								timestamp: 1483693575274,
-								downstream: []
-							},
-							{
-								name: "testkit",
-								color: "red",
-								building: false,
-								timestamp: 1483693575274,
-								downstream: []
-							},
-							{
-								name: "testR",
-								color: "blue",
-								building: false,
-								timestamp: 1483693575274,
-								downstream: []
-							}
-						]
-					}
-				]
-
-			},
-			{
-				name: "Nightly",
-				color: "red",
-				building: false,
-				timestamp: 1483693575274,
-				downstream: [
-					{
-						name: "Stress",
-						color: "blue",
-						building: false,
-						timestamp: 1483693575274,
-						downstream: [
-							{
-								name: "RunE2E",
-								color: "blue",
-								building: false,
-								timestamp: 1483693575274,
-								downstream: [
-									{
-										name: "BuildInstance",
-										color: "blue",
-										building: false,
-										timestamp: 1483693575274,
-										downstream: []
-									},
-									{
-										name: "testkit",
-										color: "red",
-										building: false,
-										timestamp: 1483693575274,
-										downstream: []
-									}
-								]
-							}
-						]
-					}
-				]
-			}
-		]
-	};
-	res.send(j);
-
-	// jenkins.job.get("Build", (err, data) => {
-	// 	if (err) throw err;
-	//
-	// 	res.send(data);
-	// });
+	jenkins.listJobsByBuild(req.params.id).then(
+		jobs => res.send(jobs),
+		err => next(err)
+	);
 });
 
 module.exports = router;
--- service/jenkinsLib.js
+++ service/jenkinsLib.js
@@ -1,30 +1,63 @@
 const config  = require("../config.json");
 const jenkins = require('jenkins')({ baseUrl: config.baseUrl, crumbIssuer: true});
 
-function listBuilds(limit, callback){
+function handleBuilds(builds, limit) {
+	return builds
+		.filter(build =>
+			build.displayName.startsWith("RC-") || build.displayName.startsWith("master-")
+		)
+		.slice(0, limit);
+}
+
+function listBuilds(limit) {
 	if (typeof limit === 'undefined') {
 		limit = 10;
 	}
-	jenkins.job.get("Build", {depth: 1, tree: "builds[displayName,number]"}, (err, data) => {
-		if (err) next(err);
 
-		callback(data.builds.filter(k => {
-			return k.displayName.startsWith("RC-") || k.displayName.startsWith("master-")
-		}).slice(0, limit).map(k => {
-			console.log(k);
-			return {name: k.displayName, number: k.number}
-		}));
-	});
+	if (!config.realEnv) {
+		return new Promise(function (resolve, reject) {
+			let data = require('../mocks/builds.json');
+			let builds = handleBuilds(data.builds, limit);
+			resolve(builds);
+		});
+	} else {
+		return new Promise(function (resolve, reject) {
+			jenkins.job.get("Build", {
+				depth: 1,
+				tree: "builds[id,displayName,number,building,timestamp,result]"
+			}, (err, data) => {
+				if (err) { reject(err); }
+				else {
+					let builds = handleBuilds(data.builds, limit);
+					resolve(builds);
+				}
+			});
+		});
+	}
 }
 
-function listJobsByBuild(number, callback){
+function listJobsByBuild(number) {
 	if(typeof number === 'undefined'){
 		throw new Error('number must be defined!');
 	}
-	jenkins.build.get("Build", 4, {depth: 2, tree: "building,duration,result,timestamp"}, (err, data) => {
-		// if (err) next(err);
-		console.log(data)
-	});
+
+	if (!config.realEnv) {
+		return new Promise(function (resolve, reject) {
+			let jobs = require('../mocks/jobs.json');
+			resolve(jobs);
+		});
+	} else {
+		return new Promise(function (resolve, reject) {
+			jenkins.build.get("Build", number, {depth: 2, tree: "building,duration,result,timestamp"}, (err, data) => {
+				if (err) {
+					reject(err);
+				}
+				else {
+					resolve(data);
+				}
+			});
+		});
+	}
 }
 
 
