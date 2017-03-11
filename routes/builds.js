524bfebf87fe9595599fa9c40de47715072109c3
Michael Vashchinsky <michaelv@sparkbeyond.com>
2017-03-11 20:41:16 +0200

    Migrate routes to async/await

--- routes/builds.js
+++ routes/builds.js
@@ -4,25 +4,33 @@ const jenkinsService = require("../service/jenkinsLib");
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	jenkinsService.listBuilds(10).then(
-		builds => res.send(builds),
-		err => next(err)
-	);
+router.get('/', async function(req, res, next) {
+	try {
+		let builds = await jenkinsService.listBuilds(10);
+		res.send(builds);
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.get('/:id', function(req, res, next) {
-	jenkinsService.listJobsByBuild(parseInt(req.params.id, 10)).then(
-		jobs => res.send(jobs),
-		err => next(err)
-	);
+router.get('/:id', async function(req, res, next) {
+	try {
+		let buildId = parseInt(req.params.id, 10);
+		let jobs = await jenkinsService.listJobsByBuild(buildId);
+		res.send(jobs);
+	} catch (err) {
+		next(err);
+	}
 });
 
-router.get('/downstream/:jobName/:buildId', function(req, res, next) {
-	jenkinsService.getDownstreamProjects(req.params.jobName, parseInt(req.params.buildId, 10)).then(
-		jobs => res.send(jobs),
-		err => next(err)
-	);
+router.get('/downstream/:jobName/:buildId', async function(req, res, next) {
+	try {
+		let buildId = parseInt(req.params['buildId'], 10);
+		let jobs = await jenkinsService.getDownstreamProjects(req.params['jobName'], buildId);
+		res.send(jobs);
+	} catch (err) {
+		next(err);
+	}
 });
 
 
